f555a2d918fd147a987ccb998519132685764458
Mansi Verma <148568768+Mansi12v@users.noreply.github.com>
2023-10-31 22:41:28 +0530

    Add files via upload

---
+++ getset.java
@@ -0,0 +1,33 @@
+public class getset {
+
+        public static void main(String args[]){
+            Pen p1 =  new Pen();
+            p1.setcolor("blue");
+            System.out.println(p1.getcolor());
+            p1.setTip (5);
+            System.out.println(p1.getTip());
+            p1.setcolor("Yellow");
+            System.out.println(p1.getcolor());
+            
+        }
+    
+        
+
+    class Pen {
+        private String color;
+        private int tip;
+          String getcolor(){
+            return this.color;
+          }
+          int getTip(){
+            return this.tip;
+          }
+        void setcolor(String newcolor){
+            this.color = newcolor;
+        }
+        void setTip(int newTip){
+            this.tip = newTip;
+        }
+    }
+    
+}
